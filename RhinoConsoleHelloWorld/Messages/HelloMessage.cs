ce283c894e11f5c8897dcc4b4ec343b2fb540d2a
Sebastián Maldonado <sebmaldo@gmail.com>
2013-02-06 16:03:53 -0300

    Configurar Colas
    Configuracion de colas para envio de mensajes.

--- RhinoConsoleHelloWorld/Messages/HelloMessage.cs
+++ RhinoConsoleHelloWorld/Messages/HelloMessage.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Messages
+﻿namespace Messages
 {
     /// <summary>
     /// Clase de firma de mensajes para ser utilizada enviando y resiviendo los mensajes de ese tipo.
