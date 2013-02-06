fe05b7602ce72c432ef3fd73d7b756086c2cd898
Sebastián Maldonado <sebmaldo@gmail.com>
2013-02-06 15:39:37 -0300

    Establecer el mensaje
    Se establece el mensaje que se va a utilizar.

--- RhinoConsoleHelloWorld/ConsoleProductor/Program.cs
+++ RhinoConsoleHelloWorld/ConsoleProductor/Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleProductor
     {
         static void Main(string[] args)
         {
+            Console.ReadLine();-
         }
     }
 }
---
+++ RhinoConsoleHelloWorld/Messages/HelloMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messages
+{
+    /// <summary>
+    /// Clase de firma de mensajes para ser utilizada enviando y resiviendo los mensajes de ese tipo.
+    /// </summary>
+    public class HelloMessage
+    {
+        /// <summary>
+        /// Mensaje para ser intercambiado entre las diferentes soluciones.
+        /// </summary>
+        public string Mensaje { get; set; }
+    }
+}
